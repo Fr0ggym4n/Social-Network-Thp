a2eb314f6790296e8c515fdacc17491a7609ce1f
Charles <charlescampet03@gmail.com>
2021-05-18 12:05:05 +0200

    Add pages and api needed

---
+++ src/api/apiHandler.js
@@ -0,0 +1,112 @@
+const BASE_URL = 'http://localhost:1337';
+
+// USER
+
+const REGISTER = (data) => ({
+    URL: `${BASE_URL}/auth/local/register`,
+    METHOD: 'post',
+    HEADERS: {
+      'Content-Type': 'application/json',
+    },
+    BODY: JSON.stringify(data),
+  });
+  
+  const LOGIN = (data) => ({
+    URL: `${BASE_URL}/auth/local`,
+    METHOD: 'post',
+    HEADERS: {
+      'Content-Type': 'application/json',
+    },
+    BODY: JSON.stringify(data),
+  });
+  
+  const OWN_PROFILE = (token) => ({
+    URL: `${BASE_URL}/users/me`,
+    METHOD: 'get',
+    HEADERS: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  const EDIT_PROFILE = (token, data) => ({
+    URL: `${BASE_URL}/users/me`,
+    METHOD: 'put',
+    HEADERS: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    BODY: JSON.stringify(data),
+  });
+  
+  const GET_USER = (token, id) => ({
+    URL: `${BASE_URL}/users/${id}`,
+    METHOD: 'get',
+    HEADERS: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  export {
+    REGISTER,
+    LOGIN,
+    OWN_PROFILE,
+    EDIT_PROFILE,
+    GET_USER,
+  };
+  
+  // POSTS
+  
+  const CREATE_POST = (token, data) => ({
+    URL: `${BASE_URL}/posts`,
+    METHOD: 'post',
+    HEADERS: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+    BODY: JSON.stringify(data),
+  });
+  
+  const GET_POSTS = {
+    URL: `${BASE_URL}/posts?_sort=created_at%3Adesc`,
+    METHOD: 'get',
+    HEADERS: {
+      'Content-Type': 'application/json',
+    },
+  };
+  
+  const POSTS_COUNT = (token) => ({
+    URL: `${BASE_URL}/posts/count`,
+    METHOD: 'get',
+    HEADERS: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  const GET_USER_POSTS = (token, userId) => ({
+    URL: `${BASE_URL}/posts?_sort=created_at%3Adesc&user=${userId}`,
+    METHOD: 'get',
+    HEADERS: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  const DELETE_POST = (token, id) => ({
+    URL: `${BASE_URL}/posts/${id}`,
+    METHOD: 'delete',
+    HEADERS: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  
+  export {
+    CREATE_POST,
+    GET_POSTS,
+    POSTS_COUNT,
+    GET_USER_POSTS,
+    DELETE_POST,
+  };
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,36 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { userLogOut } from 'store/user/userActions';
+import { removeAuthenticationCookie } from 'utils/cookieUtils';
+import './style.scss';
+
+const Header = () => {
+  const currentUser = useSelector((state) => (state.info));
+  const { username } = currentUser;
+  const dispatch = useDispatch();
+
+  const handleLogOut = () => {
+    removeAuthenticationCookie();
+    dispatch(userLogOut());
+  };
+
+  return (
+    <div className="Header">
+      <Link to="/" className="Header__title">M&C Social Network</Link>
+      {!username && (
+        <div className="Header__public">
+          <Link to="/login" className="Header__public__link">Log in</Link>
+          <Link to="/register" className="Header__public__link">Sign up</Link>
+        </div>
+      )}
+      {username && (
+        <div className="Header__loggedIn">
+          <Link to="/profile" className="Header__LoggedIn__profile">Profile</Link>
+          <button type="button" className="Header__loggedIn__logOut" onClick={handleLogOut}>Log out, {username}</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
