d52e9ecf087b1b1e2178b149aeb713d8164f506f
Charles <charlescampet03@gmail.com>
2021-05-18 10:43:08 +0200

    add base of files to work

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => (
+    <div className="App">
+        <h1> Hello World</h1>
+        <p>This is a starter for React App !</p>
+        <p>This website is a training to Redux and React. We use auth and routing to create a small social media website.</p>
+    </div>
+);
+
+export default App;
